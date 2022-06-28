0b5ded27e2af8a13938d614ffef3e40257993611
Annie  Indreiten <devannie777@gmail.com>
2022-06-28 11:45:11 +0100

    Card component with props

--- src/App.js
+++ src/App.js
@@ -2,13 +2,21 @@ import './App.css';
 import NavBar from './components/NavBar';
 import Hero from './components/Hero';
 import Card from './components/Card';
+import katie from './images/image 12.png';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Hero />
-      <Card />
+      <Card
+        img={katie}
+        rating="5.0"
+        reviewCount={6}
+        country={"USA"}
+        title="Life Lessons with Katie Zaferes"
+        price={136}
+      />
 
     </div>
   );
