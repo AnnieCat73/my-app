9057d4167aa27348b99a8fc6799cdf77132ae314
Annie  Indreiten <devannie777@gmail.com>
2022-06-29 07:07:45 +0100

    Card component styles

--- src/App.js
+++ src/App.js
@@ -2,21 +2,36 @@ import './App.css';
 import NavBar from './components/NavBar';
 import Hero from './components/Hero';
 import Card from './components/Card';
-import katie from './images/image 12.png';
+//import katie from './images/image 12.png';
+import data from './data';
 
 function App() {
+  const cards = data.map(item => {
+    return (
+      <Card
+        key={item.id}
+        /* {...item}*/
+        img={item.coverImg}
+        rating={item.stats.rating}
+        reviewCount={item.stats.rating.reviewCount}
+        country={item.location}
+        title={item.title}
+        price={item.price}
+        openSpots={item.openSpots}
+      />
+    )
+  })
   return (
     <div className="App">
       <NavBar />
       <Hero />
-      <Card
-        img={katie}
-        rating="5.0"
-        reviewCount={6}
-        country={"USA"}
-        title="Life Lessons with Katie Zaferes"
-        price={136}
-      />
+      <section className="cards-list">
+        {cards}
+      </section>
+
+
+
+
 
     </div>
   );
--- src/components/Card.js
+++ src/components/Card.js
@@ -2,8 +2,16 @@ import React from 'react'
 import star1 from '../images/Star 1.png';
 
 function Card(props) {
+  //console.log(props.openSpots)
+  let badgeText;
+  if (props.openSpots === 0) {
+    badgeText = "SOLD OUT";
+  } else if (props.location === "Online") {
+    badgeText = "ONLINE";
+  }
   return (
     <div className="card">
+      {badgeText === 0 && <div className="card-badge">{badgeText}</div>}
       <img className="image" src={props.img} alt="" />
       <div className="card-stats">
         <img className="card-star" src={star1} alt="" />
@@ -13,6 +21,7 @@ function Card(props) {
       </div>
       <p>{props.title}</p>
       <p><span className="bold">From ${props.price}</span> / per person</p>
+
     </div>
   )
 }
